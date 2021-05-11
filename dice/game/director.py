8993f57f6bc5b42962e9f8b3db7137336c16370e
TheStarCorner <ammonwilson1@gmail.com>
2021-05-11 13:44:19 -0600

    testing my name comment.

--- dice/game/director.py
+++ dice/game/director.py
@@ -1,5 +1,5 @@
 from game.thrower import Thrower
-
+# Ammon
 class Director:
     """A code template for a person who directs the game. The responsibility of 
     this class of objects is to keep track of the score and control the 
